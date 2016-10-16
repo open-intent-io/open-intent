f4008a81fa73f5a17d468518cf645b5d331c62ac
Clement Michaud <clement.michaud34@gmail.com>
2016-10-16 15:46:46 +0200

    Package javascript code in tar.gz and deploy it in integration tests to test npm publish

--- bindings/nodejs/docker/entrypoint.sh
+++ bindings/nodejs/docker/entrypoint.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+PACKAGE_TARBALL=/tmp/open-intent.tgz
+
 if [ ! -n "$(ls -A /chatbot)" ]
 then
     echo "New chatbot detected, initializing resources..."
 
     cd /tmp
-    open-intent project create chatbot > /dev/null 2>&1
+
+    echo "Creating chatbot project"
+    open-intent project create --no-npm chatbot > /dev/null 2>&1
+
+    cd chatbot
+
+    echo "Install the packages"
+    npm install ${PACKAGE_TARBALL}
+    npm install
+
+    echo "Move chatbot project to /chatbot"
     mv /tmp/chatbot/* /chatbot
 
-    cd /chatbot
+    echo "Chatbot project is ready"
 else
     echo "Existing chatbot detected."
 fi
-
-open-intent project start chatbot
